28be28bc45d71222afe458ae5e719beea169e1ad
aliciawilliams7914 <mykitchendesignsolution@gmail.com>
2026-08-19 08:07:48 -0400

    changes

---
+++ index.js
@@ -0,0 +1,22 @@
+// template_6woheuf
+// aliciawilliams7914
+DQkaMsn_hRekzl4C8
+<script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
+        </script>
+        <script type="text/javascript">   
+        (function(){      
+            emailjs.init({        
+            publicKey: "YOUR_PUBLIC_KEY",      
+        });   
+        })();
+        </script>
+
+
+
+
+
+
+
+
+
