d978b9ba63041f1d73c202e556cfa26952dae66b
aliciawilliams7914 <mykitchendesignsolution@gmail.com>
2026-08-19 17:34:27 -0400

    changes

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-// template_6woheuf
-// aliciawilliams7914
-// user_DQkaMsn_hRekzl4C8
-
 function contact(event) {
-    event.preventDefault();
-    // emailjs
-    // .sendForm(
-    // 'service_aliciawilliams7914',
-    // 'template_6woheuf',
-    // event.target,
-    // 'user_DQkaMsn_hRekzl4C8'
-// ).then(() => {
-    // console.log('this worked')
-// })
+event.preventDefault();
 const loading = document.querySelector('. modal__overlay--loading')
 const success = document.querySelector('.modal__overlay--success')
-loading.classList += " modal__overlay--visible"
-SetTimeout(() => {
-    loading.classList.remove("modal__overlay--visible"); 
-    success.classList += " modal__overlay--visible";
+loading.classList += " modal__overlay--visible";
 
-    console.log('it worked 1') 
-}, 1000);
 
+    emailjs
+    .sendForm(
+    'service_aliciawilliams7914',
+    'template_6woheuf',
+    event.target,
+    'user_DQkaMsn_hRekzl4C8'
+
+).then(() => { 
+loading.classList.remove("modal__overlay--visible"); 
+success.classList += " modal__overlay--visible";
+
+}).catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert( 
+        "The email service is temporarily unavailable. Please contact me directly at alicia.williams219@gmail.com"
+    );
+})
 }
 
 
