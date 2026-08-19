ee1fd9ef40e7c341bb59781884d478127da0a7d5
aliciawilliams7914 <mykitchendesignsolution@gmail.com>
2026-08-19 17:05:43 -0400

    changes

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 // template_6woheuf
 // aliciawilliams7914
-DQkaMsn_hRekzl4C8
-<script type="text/javascript"
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
-        </script>
-        <script type="text/javascript">   
-        (function(){      
-            emailjs.init({        
-            publicKey: "YOUR_PUBLIC_KEY",      
-        });   
-        })();
-        </script>
+// user_DQkaMsn_hRekzl4C8
+
+function contact(event) {
+    event.preventDefault();
+    // emailjs
+    // .sendForm(
+    // 'service_aliciawilliams7914',
+    // 'template_6woheuf',
+    // event.target,
+    // 'user_DQkaMsn_hRekzl4C8'
+// ).then(() => {
+    // console.log('this worked')
+// })
+const loading = document.querySelector('. modal__overlay--loading')
+const success = document.querySelector('.modal__overlay--success')
+loading.classList += " modal__overlay--visible"
+SetTimeout(() => {
+    loading.classList.remove("modal__overlay--visible"); 
+    success.classList += " modal__overlay--visible";
+
+    console.log('it worked 1') 
+}, 1000);
+
+}
+
+
+
 
 
 
