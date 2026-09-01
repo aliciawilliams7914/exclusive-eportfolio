81a851e5633f2486a288e444ceac3068b963a5c2
aliciawilliams7914 <mykitchendesignsolution@gmail.com>
2026-08-31 22:30:12 -0400

    changes

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = 'translate(${x * boolInt}px, ${y * boolInt}px)'
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+    document.body.classList.remove("dark-theme")
+
+}
+}
+
 function contact(event) {
 event.preventDefault();
 const loading = document.querySelector('.modal__overlay--loading');
@@ -17,10 +44,10 @@ emailjs
     alert( 
         "The email service is temporarily unavailable. Please contact me directly at alicia.williams219@gmail.com"
     );
-})
+});
 }
  
-let isModalOpen = false;
+
 function toggleModal() {
      if (isModalOpen) {
         isModalOpen = false;
